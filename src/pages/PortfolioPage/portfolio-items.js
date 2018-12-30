7cfd053c1aa1bede8a1ca7503ac81e2a77425e3c
tomcheah <tomcheah@gmail.com>
2018-12-30 14:01:31 -0800

    Update portfolio item links and descriptions

--- src/pages/PortfolioPage/portfolio-items.js
+++ src/pages/PortfolioPage/portfolio-items.js
@@ -8,16 +8,23 @@ export default [{
         Berkeley Mobile
       </div>
       <div className='portfolio-item__desc'>
-        Web scrapers to retrieve data from UC Berkeley websites
+      	App to help UC Berkeley students discover campus resources
       </div>
+      <br />
+      <div className='portfolio-item__desc'>
+        Implemented web scrapers to retrieve data from UC Berkeley websites
+      </div>
+
       <div className='portfolio-item__icon'>
         <i className="fab fa-python"></i>
         <i className="fab fa-android"></i>
         <i className="fab fa-apple"></i>
       </div>
       <div className='portfolio-item__links'>
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/asuc-octo/asuc-backend">More</a>
-      </div>
+		<a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.asuc.asucmobile&hl=en">Play Store</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://lh3.googleusercontent.com/mIqZ2vnwvs8jHjCuEVqHB4t5hMF_SsZQ9SAFynxiysK0rOL3u26wYZ8BZVhJ773r6qI=w952-h968-rw">Image</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://itunes.apple.com/us/app/berkeley-mobile/id912243518?mt=8">App Store</a>      
+       </div>
     </div>
   )
 }, {
@@ -27,6 +34,10 @@ export default [{
       <div className='portfolio-item__title'>
         AFX Dance Website
       </div>
+       <div className='portfolio-item__desc'>
+      	Website to capture the history of a student dance organization
+      </div>
+      <br />
       <div className='portfolio-item__desc'>
         Implemented an image hover feature to display histories of members
       </div>
@@ -35,7 +46,7 @@ export default [{
         <i className="fab fa-js"></i>
       </div>
       <div className='portfolio-item__links'>
-        <a target="_blank" rel="noopener noreferrer" href="https://afx.dance/">More</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://afx.dance/">Link</a>
       </div>
     </div>
   )
@@ -46,6 +57,10 @@ export default [{
       <div className='portfolio-item__title'>
         AFX Dance Auditions App
       </div>
+ 	  <div className='portfolio-item__desc'>
+      	App to help directors manage dance auditions and organize team rosters
+      </div>
+      <br />
       <div className='portfolio-item__desc'>
         Implemented the backend logic for team switches
       </div>
@@ -53,7 +68,8 @@ export default [{
         <i className="fab fa-chrome"></i>
       </div>
       <div className='portfolio-item__links'>
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/afxdance/auditions-app">More</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://i.imgur.com/Ts8ovL9.png">Image</a>
+        <a target="_blank" rel="noopener noreferrer" href="http://roster.afx.dance/teamswitch">Link</a>
       </div>
     </div>
   )
